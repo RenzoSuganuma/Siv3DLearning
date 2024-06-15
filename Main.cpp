f8964485ebc0f01acfccf129706cd510f125816f
MSI_LapTop_RenzoSuganuma <103351677+RenzoSuganuma@users.noreply.github.com>
2024-06-15 12:23:23 +0900

    マーク路

--- Main.cpp
+++ Main.cpp
@@ -3,25 +3,13 @@
 #include "functional"
 #include "list"
 
-UserGameFoundation* game = new UserGameFoundation();
+#define CLOG( message ) Console << U ## message;
 
-std::list<std::function<void(void)>> func_list;
+UserGameFoundation* game = new UserGameFoundation();
 
 void Main()
 {
-	func_list.emplace_back([]() {Console << U"p"; });
-	func_list.emplace_back([]() {Console << U"p1"; });
-	func_list.emplace_back([]() {Console << U"p2"; });
-	func_list.emplace_back([]() {Console << U"p3"; });
-	func_list.emplace_back([]() {Console << U"p4"; });
-	func_list.emplace_back([]() {Console << U"p5"; });
-	func_list.emplace_back([]() {Console << U"p6"; });
-
-	auto itr = func_list.begin();
-
-	Console << &(*itr);
-	itr++;
-	Console << &(*itr);
+	CLOG("o");
 
 	game->Setup();
 	game->Validation();
